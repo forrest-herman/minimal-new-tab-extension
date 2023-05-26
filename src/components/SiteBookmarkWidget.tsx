1b868fc6f62e4581892c48b9b2bdd75dde4911a1
forrest-herman <forrestherman13@gmail.com>
2023-05-25 22:12:54 -0400

    add rough fetch of favicon for websites

--- src/components/SiteBookmarkWidget.tsx
+++ src/components/SiteBookmarkWidget.tsx
@@ -9,9 +9,15 @@ type SiteBookmarkWidgetProps = {
 export function SiteBookmarkWidget({ siteUrl, name, fontSize, iconSize, settingsOpen }: SiteBookmarkWidgetProps) {
   const openWebPage = () => {!settingsOpen && window.open(siteUrl, "_self")}
 
+  const getFavicon = () => {
+    if (siteUrl === "") 
+      return "src/logos/bookmark.ico"
+    return `https://t3.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&url=${siteUrl}&size=256`
+  }
+
   return (
     <div>
-      <img width={iconSize} height={iconSize} src="src\logos\insta icon.png" alt="" onClick={() => openWebPage()} />
+      <img width={iconSize} height={iconSize} src={getFavicon()} alt="" onClick={() => openWebPage()} style={{cursor:'pointer'}}/>
       <br/>
       {name &&<span style={{
           fontSize: fontSize * 4 + "px",
